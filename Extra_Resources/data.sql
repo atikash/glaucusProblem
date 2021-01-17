74c56b80c74db61f712fcd0eab1f45f9b049451a
atikash <atikash@users.noreply.github.com>
2021-01-17 15:24:01 +0530

    Update data.sql

--- Extra_Resources/data.sql
+++ Extra_Resources/data.sql
@@ -1,5 +1,5 @@
 create database glaucusproblem;
 use glaucusproblem;
 create table number(id BigInt(20),number BigInt(20));
-delete * from number;
+delete from number;
 insert into number values(0,0);
